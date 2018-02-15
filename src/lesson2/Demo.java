1a40f03c1145f261891bf2d20e22a24e34c8c923
YuriiS-68 <yurii_s@bigmir.net>
2018-02-15 13:26:48 +0200

    add home work to lesson4 sql

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,15 +1,19 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         //java
         //1. create class with fields +
-        //2. create object with some values
-        //3. using it
+        //2. create object with some values +
+        //3. using it +
+
+        Order order = new Order(1001, "tool2212", 140, new Date(), new Date());
 
         //db
-        //1. create table with columns -
-        //2. insert some data to the table
-        //3. using it
+        //1. create table with columns +
+        //2. insert some data to the table +
+        //3. using it +
     }
 }
